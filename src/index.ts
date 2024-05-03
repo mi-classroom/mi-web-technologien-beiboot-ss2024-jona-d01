21abd771cba070877dc13a41b3d059583ffd85dc
JonaDielmann <jona.dielmann@deutschebahn.com>
2024-05-03 17:24:19 +0200

    add swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { router } from "./routes.ts";
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDoc from '../swagger.json';
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(router);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
